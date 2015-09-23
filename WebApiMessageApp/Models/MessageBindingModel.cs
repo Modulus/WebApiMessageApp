f1f25c0ea31b5a6b61d2ddc0f29d993477dc833d
Modulus <thejohnskauge@gmail.com>
2015-09-23 16:34:00 +0200

    Added MessageBinding and viemodel

---
+++ WebApiMessageApp/Models/MessageBindingModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApiMessageApp.Models
+{
+    public class MessageBindingModel
+    {
+        [Required]
+        [Display(Name = "Subject")]
+        public string Subject { get; set; }
+
+        [Required]
+        [Display(Name = "Message body")]
+        [DataType(DataType.MultilineText)]
+        [StringLength(255, ErrorMessage = "The {0} must be at least {2} characters long", MinimumLength =5)]
+        public string Message { get; set; }
+    }
+}
---
+++ WebApiMessageApp/Models/MessageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiMessageApp.Models
+{
+    public class MessageViewModel
+    {
+        public string Id { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
